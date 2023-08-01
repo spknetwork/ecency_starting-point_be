d63711b19df8fd73dfea3aa256e11017aef6644d
PijusSerapinasMipsas <pijus.s@mipsas.com>
2023-08-01 15:01:57 +0300

    added logging middleware and cors

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/spknetwork/ecency_starting-point_be/routes"
 )
 
 func main() {
     app := fiber.New()
+    app.Use(logger.New(logger.Config{
+    Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+    }))
+    app.Use(cors.New(cors.Config{
+        AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+        AllowOrigins:     "*",
+        AllowCredentials: true,
+        AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+    }))
     routes.SetupRoutes(app)
     app.Listen(":8000")
 }
